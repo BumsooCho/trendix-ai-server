d3d8c57c8c4bc5d5a0546ef20d11d135862fb4b8
PJE <donangyi64@gmail.com>
2026-01-13 21:57:20 +0900

    fix:[박중은] port 원복

--- app/main.py
+++ app/main.py
@@ -102,5 +102,5 @@ if __name__ == "__main__":
     import uvicorn
 
     host = os.getenv("APP_HOST", "0.0.0.0")
-    port = int(os.getenv("APP_PORT", "33333"))
+    port = int(os.getenv("APP_PORT", "8000"))
     uvicorn.run(app, host=host, port=port)
